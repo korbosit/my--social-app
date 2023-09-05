6c3497bd7dcdd3384f6fcae3471d95774aa13e06
Vlaimir Korba <krissky1986@gmail.com>
2023-09-05 16:21:46 +0300

    разбивка на компоненты

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+
+
+const Profile = () => {
+   return (
+		<div className='content'>
+			<div>
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9u6_4OALQDJaY9EPmzp8E-35kSySl_vWqmg&usqp=CAU" />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>
+						post 1
+					</div>
+					<div>
+						post 2
+					</div>
+				</div>
+			</div>
+		</div>
+   );
+};
+
+export default Profile;
