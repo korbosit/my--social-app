bbe3bb0553794dee6a3759d28ce76a91004c6c66
Vlaimir Korba <krissky1986@gmail.com>
2023-09-06 20:19:15 +0300

    20-Navlink

--- src/App.js
+++ src/App.js
@@ -8,29 +8,13 @@ import { Routes, Route, BrowserRouter, } from 'react-router-dom';
 import './App.css';
 
 
-
-// const App =() => {
-// 	return (
-// 	<BrowserRouter>
-// 		<div className="app-wrapper">
-// 			<Header />
-// 			<Navbar />
-// 				<div className="app-wrapper-content">
-// 					<Route component={Dialogs}/>
-// 					<Route component={Profile}/>
-// 				</div>
-// 			</div>
-// 	</BrowserRouter>
-// 	);
-// }
-
 const App = () => {
    return (
 		<BrowserRouter>
         <div className='app-wrapper'>
               <Header />
               <Navbar />
-         <div class='app-wrapper-content'>
+         <div className='app-wrapper-content'>
              <Routes>
                  <Route path="/dialog" element= {<Dialogs/>}/>
                  <Route path="/profile" element={<Profile/>}/>
