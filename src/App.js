38d950d787cea33a47c91c03cf3b396763208237
Vlaimir Korba <krissky1986@gmail.com>
2023-09-07 11:54:36 +0300

    22-route-exact

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ import './App.css';
 
 
 const App = () => {
-   return (
-		<BrowserRouter>
-        <div className='app-wrapper'>
-              <Header />
-              <Navbar />
-         <div className='app-wrapper-content'>
-             <Routes>
-                 <Route path="/dialog" element= {<Dialogs/>}/>
-                 <Route path="/profile" element={<Profile/>}/>
-             </Routes>
-         </div>
-        </div>
-     </BrowserRouter>
-   );
+return (
+	<BrowserRouter>
+		<div className='app-wrapper'>
+				<Header />
+				<Navbar />
+		<div className='app-wrapper-content'>
+				<Routes>
+					{/* <Route path="/dialogs" element= {<Dialogs/>}/> */}
+					<Route path="/dialogs/*" element={<Dialogs />} /> 
+					<Route path="/profile" element={<Profile/>}/>
+				</Routes>
+		</div>
+		</div>
+	</BrowserRouter>
+);
 };
 
 export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,38 @@
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-	return (
-		<div>
-			Dialogs
-		</div>
-	);
+   return (
+      <div>
+         <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+               <div className={s.dialog + ' ' + s.active}>
+						<NavLink to="/dialogs/1">Vovanec</NavLink>
+					</div>
+               <div className={s.dialog}>
+						<NavLink to="/dialogs/2">Sveta</NavLink>
+					</div>
+               <div className={s.dialog}>
+						<NavLink to="/dialogs/3">Alfa</NavLink>
+					</div>
+               <div className={s.dialog}>
+						<NavLink to="/dialogs/4">Beta</NavLink>
+					</div>
+               <div className={s.dialog}>
+						<NavLink to="/dialogs/5">Mikki</NavLink>
+					</div>
+               <div className={s.dialog}>
+						<NavLink to="/dialogs/6">Timka</NavLink>
+					</div>
+            </div>
+            <div className={s.messages}>
+               <div className={s.message}>Hi</div>
+               <div className={s.message}>How are you?</div>
+               <div className={s.message}>Yo-yo-yo</div>
+            </div>
+         </div>
+      </div>
+   );
 }
 
 export default Dialogs;
