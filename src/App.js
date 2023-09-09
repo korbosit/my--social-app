c5b21ac242105b297edf5927ebd1d1e55cd49348
Vlaimir Korba <krissky1986@gmail.com>
2023-09-09 15:59:24 +0300

    31. Уроки React JS - onClick, ref, VirtualDOM

--- src/App.js
+++ src/App.js
@@ -11,38 +11,35 @@ import './App.css';
 const App = (props) => {
 
 return (
-   <BrowserRouter>
-      <div className="app-wrapper">
-         <Header />
-         <Navbar />
-         <div className="app-wrapper-content">
-            <Routes>
-               {/* <Route path="/dialogs/*" render={() => <Dialogs />} />
-               <Route path="/dialogs/*" render={() => <Profile />} /> */}
-					
-					
-               {/* <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
-               <Route path="/profile" element={<Profile posts={props.posts} />} /> */}
+	<div className="app-wrapper">
+	<Header />
+	<Navbar />
+	<div className="app-wrapper-content">
+		<Routes>
+			{/* <Route path="/dialogs/*" render={() => <Dialogs />} />
+			<Route path="/dialogs/*" render={() => <Profile />} /> */}
+			
+			
+			{/* <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
+			<Route path="/profile" element={<Profile posts={props.posts} />} /> */}
 
-               {/* <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
-               <Route path="/profile" element={<Profile posts={props.posts} />} /> */}
+			{/* <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
+			<Route path="/profile" element={<Profile posts={props.posts} />} /> */}
 
 
-               {/* <Route path="/dialogs/*" element={<Dialogs 
-																	dialogs={props.state.dialogsPages.dialogs}
-																	messages={props.state.dialogsPages.messages} />} />
+			{/* <Route path="/dialogs/*" element={<Dialogs 
+															dialogs={props.state.dialogsPages.dialogs}
+															messages={props.state.dialogsPages.messages} />} />
 
-               <Route path="/profile" element={<Profile posts={props.state.profilePage.posts} />} /> */}
+			<Route path="/profile" element={<Profile posts={props.state.profilePage.posts} />} /> */}
 
-               <Route path="/dialogs/*" element={<Dialogs 
-																	state={props.state.dialogsPages} />} />
+			<Route path="/dialogs/*" element={<Dialogs 
+															state={props.state.dialogsPages} />} />
+			<Route path="/profile" element={<Profile state={props.state.profilePage} />} />
 
-               <Route path="/profile" element={<Profile state={props.state.profilePage} />} />
-
-            </Routes>
-         </div>
-      </div>
-   </BrowserRouter>
+		</Routes>
+	</div>
+</div>
 );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './Redux/state';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App posts={posts} dialogs={dialogs} messages={messages}/> */}
-    <App state={state}/>
-  </React.StrictMode>
+   <React.StrictMode>
+      <BrowserRouter>
+         {/* <App posts={posts} dialogs={dialogs} messages={messages}/> */}
+         <App state={state} />
+      </BrowserRouter>
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
