4502310e966e3bfe4f3ed68a1c020110d525ab5a
Vlaimir Korba <krissky1986@gmail.com>
2023-09-16 12:26:12 +0300

    39-Курс React JS - action creator, action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../Redux/state';
 
 
 const MyPosts = (props) => {
@@ -10,16 +10,13 @@ const MyPosts = (props) => {
 	let newPostElement = React.createRef();
 
 	let addPost = () => {
-		// let text = newPostElement.current.value
-		// прокинутая функция из state
-		// props.addPost(text);
-		props.dispatch({ type:'ADD-POST' });
-		// props.updateNewPostText('');
+		props.dispatch(addPostActionCreator());
 	}
 
 	let onPostChange = () => {
 		let text = newPostElement.current.value;
-		let action = ({ type:'UPDATE-NEW-POST-TEXT', newText: text });
+		// let action = ({ type:'UPDATE-NEW-POST-TEXT', newText: text });
+		let action = updateNewPostTextActionCreator(text);
 		props.dispatch(action)
 	}
 
